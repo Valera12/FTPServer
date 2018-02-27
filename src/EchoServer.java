50ed9315f645e704e515ce7ac1835b62f2ce22c4
Valera Tikavy <whiteleone6@gmail.com>
2018-02-27 19:04:35 +0300

    add function of connect & disconect to client client

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,33 +1,60 @@
+import javax.swing.*;
 import java.net.*;
 import java.io.*;
 
 public class EchoServer {
 
+    private final String CRLF = "\r\n";
+    private ServerSocket serverSocket;
+    private Socket incoming;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+    private PrintWriter out;
+
+    private void init() throws IOException {
+        serverSocket = new ServerSocket(8189);
+        incoming =  serverSocket.accept();
+        reader = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
+        writer = new BufferedWriter(new OutputStreamWriter(incoming.getOutputStream()));
+        out = new PrintWriter(incoming.getOutputStream(), true);
+    }
+
+    private void sendMessage(String message) {
+        out.write(message + CRLF);
+        out.flush();
+        System.out.println(message);
+    }
+
+    void sendWelcomeMessage() {
+        sendMessage("220 HELLO");
+    }
+
     public static void main(String[] args) {
-        try{
-            ServerSocket serverSocket = new ServerSocket(8189);
+        EchoServer server = new EchoServer();
 
-            Socket incoming = serverSocket.accept();
+        try {
+            server.init();
+            server.sendWelcomeMessage();
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
+            boolean done = true;
+            while (done) {
+                String msgFromClient = server.reader.readLine();
 
-            PrintWriter out = new PrintWriter(incoming.getOutputStream(), true);
-            //out.println("Hello! Enter BYE to exit.");
+                System.out.println(msgFromClient);
 
-            boolean done = false;
-            while (!done){
-             String msgFromClient = in.readLine();
-             if(msgFromClient == null)
-                 done = true;
-             else{
-                 out.println("Server: " + msgFromClient);
-                 if(msgFromClient.trim().equals("exit"))
-                     done = true;
-             }
+                if (msgFromClient.trim().equals("QUIT"))
+                    done = false;
+
+                if(msgFromClient.startsWith("USER")) {
+                    server.sendMessage("331 user correct");
+                }
+                if(msgFromClient.startsWith("PASS")) {
+                    server.sendMessage("230 authorized");
+                }
+
             }
-            incoming.close();
-        }
-        catch (Exception e){
+            server.incoming.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
