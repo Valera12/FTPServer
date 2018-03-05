e7dbd163e4843417174190391264f7d9d411ac2e
Valera Tikavy <whiteleone6@gmail.com>
2018-03-05 20:07:04 +0300

    implemented CWD command & STOR command

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -12,10 +12,11 @@ public class EchoServer {
     private BufferedReader reader;
     private BufferedWriter writer;
     private PrintWriter out;
-    int rowCount;
-    UsersDB usersDB;
-    String lastUsername;
-    Window window;
+    private int rowCount;
+    private String dir;
+    private UsersDB usersDB;
+    private String lastUsername;
+    private Window window;
 
 
     public EchoServer(UsersDB usersDB, Window window) {
@@ -42,6 +43,7 @@ public class EchoServer {
 
     void sendWelcomeMessage() {
         sendMessage("220 HELLO");
+        window.msgToTextArea("220 HELLO");
     }
 
     public void processRequests() {
@@ -50,8 +52,6 @@ public class EchoServer {
             while (done) {
                 String msgFromClient = reader.readLine();
                 dispatchMessage(msgFromClient);
-                window.msgToTextArea(msgFromClient);
-
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -60,21 +60,64 @@ public class EchoServer {
 
     void dispatchMessage(String message) {
         System.out.println(message);
-        window.msgToTextArea(message);
-        if (message.trim().equals("QUIT")) {
-            sendMessage("221 GOODBYE");
+        //window.msgToTextArea(message);
+
+        commandQuit(message);
+        commandPasv(message);
+        commandPass(message);
+        commandList(message);
+        commandChangeWorkDirectory(message);
+    }
+
+    private String listOfFiles(String dir) {
+        File[] filesList;
+        File dirDescr = new File(dir);
+
+        filesList = dirDescr.listFiles();
+        StringBuilder builder = new StringBuilder();
+
+        if (filesList != null) {
+            for (File file : filesList) {
+                builder.append(file.length()).append(" ").append(file.getName()).append("\n");
+            }
+        }
+
+        return builder.toString();
+    }
+
+
+    void commandPasv(String message) {
+        if (message.startsWith("PASV")) {
             try {
-                incoming.close();
+                dataServerSocket = new ServerSocket(228, 10, Inet4Address.getLocalHost());
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.exit(0);
+            int i;
+            int j;
+            String s = Integer.toBinaryString(dataServerSocket.getLocalPort());
+            //System.out.println(s);
+            StringBuilder builder = new StringBuilder();
+            for (int index = 0; index < 16 - s.length(); ++index) {
+                builder.append("0");
+            }
+            builder.append(s);
+
+            i = Integer.parseInt(builder.toString().substring(0, 8), 2);
+            System.out.println(i);
+            j = Integer.parseInt(builder.toString().substring(8), 2);
+            System.out.println(j);
+            sendMessage("227 Entering Passive Mode (192,168,0,103," + i + "," + j + ")");
+            window.msgToTextArea("227 Entering Passive Mode (192,168,0,103," + i + "," + j + ")");
         }
+    }
+
+    void commandUser(String message) {
         rowCount = usersDB.getRowCount();
         if (message.startsWith("USER")) {
             boolean found = false;
             lastUsername = message.substring(5);
-            for(int i = 0; i < rowCount; i++) {
+            for (int i = 0; i < rowCount; i++) {
                 if (usersDB.getValueAt(i, 0).equals(lastUsername)) {
                     found = true;
                     break;
@@ -83,15 +126,33 @@ public class EchoServer {
 
             if (found) {
                 sendMessage("331 Password required for " + lastUsername);
+                window.msgToTextArea("331 Password required for " + lastUsername);
             } else {
                 sendMessage("530 not logged in");
+                window.msgToTextArea("530 not logged in");
             }
 
         }
+    }
 
+    void commandQuit(String message) {
+        if (message.trim().equals("QUIT")) {
+            sendMessage("221 GOODBYE");
+            window.msgToTextArea("221 GOODBYE");
+            try {
+                incoming.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.exit(0);
+        }
+        commandUser(message);
+    }
+
+    void commandPass(String message) {
         if (message.startsWith("PASS")) {
             boolean found = false;
-            for(int i = 0; i < rowCount; i++) {
+            for (int i = 0; i < rowCount; i++) {
                 if (usersDB.getValueAt(i, 1).equals(message.substring(5)) &&
                         usersDB.getValueAt(i, 0).equals(lastUsername)) {
                     found = true;
@@ -100,49 +161,24 @@ public class EchoServer {
             }
             if (found) {
                 sendMessage("230 Logged on");
+                window.msgToTextArea("230 Logged on");
             } else {
                 sendMessage("332 Incorrect password");
+                window.msgToTextArea("332 Incorrect password");
             }
-
         }
+    }
 
-        if (message.startsWith("PASV")) {
+    void commandList(String message) {
+        if (message.startsWith("LIST")) {
             try {
-                dataServerSocket = new ServerSocket(228, 10, Inet4Address.getLocalHost());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            /*int start = dataServerSocket.toString().indexOf('/');
-            int finish = dataServerSocket.toString().indexOf(',');
-            dataServerSocket.toString().substring(start + 1, finish);*/
-            int i;
-            int j;
-            String s = Integer.toBinaryString(dataServerSocket.getLocalPort());
-            System.out.println(s);
-            StringBuilder builder = new StringBuilder();
-            for (int index = 0; index < 16 - s.length(); ++index){
-                builder.append("0");
-            }
-            builder.append(s);
 
-            i = Integer.parseInt(builder.toString().substring(0, 8),2);
-            System.out.println(i);
-            j = Integer.parseInt(builder.toString().substring(8),2);
-            System.out.println(j);
-            sendMessage("227 Entering Passive Mode (192,168,0,103,"+i+","+j+")");
-
-        }
-
-        if(message.startsWith("LIST")){
-            try {
-                String dir;
-                if(message.length() > 5) {
+                if (message.length() > 5) {
                     dir = message.substring(5);
-                    String fileInfo = List(dir);
+                    String fileInfo = listOfFiles(dir);
                     sendMessage("150 Opening data chanel for directory listing of \"" + dir + "\"");
-
+                    window.msgToTextArea("150 Opening data chanel for directory listing of \"" + dir + "\"");
                     dataSocket = dataServerSocket.accept();
-
                     out = new PrintWriter(dataSocket.getOutputStream(), true);
                     /*BufferedOutputStream out2 = new BufferedOutputStream(dataSocket.getOutputStream());
                     out2.write(fileInfo.getBytes());
@@ -151,17 +187,19 @@ public class EchoServer {
                     out.write(fileInfo + CRLF);
                     out.close();
                     sendMessage("226 Successfully transferred \"" + dir + "\"");
-                }else {
+                    window.msgToTextArea("226 Successfully transferred \"" + dir + "\"");
+                } else {
                     dir = "D:\\";
-                    String fileInfo = List(dir);
+                    String fileInfo = listOfFiles(dir);
                     sendMessage("150 Opening data chanel for directory listing of \"" + dir + "\"");
-
+                    window.msgToTextArea("150 Opening data chanel for directory listing of \"" + dir + "\"");
                     dataSocket = dataServerSocket.accept();
                     out = new PrintWriter(dataSocket.getOutputStream(), true);
                     //out.print(fileInfo + CRLF);
                     out.write(fileInfo + CRLF);
                     out.close();
                     sendMessage("226 Successfully transferred \"" + dir + "\"");
+                    window.msgToTextArea("226 Successfully transferred \"" + dir + "\"");
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -169,19 +207,28 @@ public class EchoServer {
         }
     }
 
-     private String List(String dir){
-        File []filesList;
-        File dirDescr = new File(dir);
+    void commandChangeWorkDirectory(String message) {
+        if (message.startsWith("CWD")) {
+            try {
+                dir = message.substring(4);
+                sendMessage("ZXC Directory changed to " + dir);
 
-        filesList = dirDescr.listFiles();
-        StringBuilder builder = new StringBuilder();
-
-         if (filesList != null) {
-             for (File file : filesList) {
-                 builder.append(file.length()).append(" ").append(file.getName()).append("\n");
-             }
-         }
-
-         return builder.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    //TODO: доделать команду отправки файли клиенту.
+    void commandStor(String message) {
+        if (message.startsWith("STOR")) {
+            try {
+                dataServerSocket = new ServerSocket();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String filenameToStor;
+            filenameToStor = message.substring(5);
+            listOfFiles(filenameToStor);
+        }
     }
 }
